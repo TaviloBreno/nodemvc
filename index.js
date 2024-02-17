6a51dc9c76a085ffb1fc7baa729d4d189af2deed
Tavilo Breno <agenciabweb@gmail.com>
2024-02-17 17:01:07 -0300

    Criando o modelo Task

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const hbs = exphbs.create();
 
 const conn = require('./db/conn');
 
+const Task = require('./models/Task');
+
 app.engine("handlebars", hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -16,4 +18,8 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.listen(3000);
+conn
+  .sync()
+  .then(app.listen(3000))
+  .catch((err) => console.log(err));
+
